83c646b79e52c40637046fde1acb6a1aa429d911
alexandro <alexandroyedra@gmail.com>
2021-04-21 22:41:13 -0700

    modelo de usuario

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const { model, Schema } = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+  name: String,
+  passwordHash: String,
+  notes: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Note'
+  }]
+})
+
+userSchema.plugin(uniqueValidator)
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = model('User', userSchema)
+
+module.exports = User
