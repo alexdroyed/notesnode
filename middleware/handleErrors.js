01e3836396286ab97eb03ff2336bde0ab94aa7ff
alexandro <alexandroyedra@gmail.com>
2021-04-21 22:43:21 -0700

    mejorando manejo de errores

--- middleware/handleErrors.js
+++ middleware/handleErrors.js
@@ -1,7 +1,16 @@
+const ERROR_HANDLERS = {
+  CastError: res =>
+    res.status(400).send({ id: 'id malformed' }),
+
+  JsonWebTokenError: res =>
+    res.status(401).json({ error: 'token missing or invalid' }),
+
+  defaulError: res =>
+    res.status(500).end()
+}
+
 module.exports = (error, request, response, next) => {
-  if (error.name === 'CastError') {
-    response.status(400).send({ id: 'id malformed' })
-  } else {
-    response.status(500).end()
-  }
+  const handler = ERROR_HANDLERS[error.name] || ERROR_HANDLERS.defaulError
+
+  handler(response, error)
 }
